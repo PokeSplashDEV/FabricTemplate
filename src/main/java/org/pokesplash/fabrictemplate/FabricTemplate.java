d68ef97334b48023ec001e19b44204d338e6d547
bencrow11 <bencrow11@gmail.com>
2023-10-15 19:58:31 +0100

    Initialised lang.

--- src/main/java/org/pokesplash/fabrictemplate/FabricTemplate.java
+++ src/main/java/org/pokesplash/fabrictemplate/FabricTemplate.java
@@ -6,12 +6,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.pokesplash.fabrictemplate.command.CommandHandler;
 import org.pokesplash.fabrictemplate.config.Config;
+import org.pokesplash.fabrictemplate.config.Lang;
 
 public class FabricTemplate implements ModInitializer {
 	public static final String MOD_ID = "FabricTemplate";
 	public static final String BASE_PATH = "/config/" + MOD_ID + "/";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final Config config = new Config();
+	public static final Lang lang = new Lang();
 
 	/**
 	 * Runs the mod initializer.
@@ -24,5 +26,6 @@ public class FabricTemplate implements ModInitializer {
 
 	public static void load() {
 		config.init();
+		lang.init();
 	}
 }
